4cb5c838db726bd1bd8496175a3a2968dff5e541
domce23 <domas.domas57@gmail.com>
2026-01-21 15:49:47 +0200

    Render FeatureShowcase after Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import FeatureShowcase from "@/components/FeatureShowcase";
 import Features from "@/components/Features";
 import CTA from "@/components/CTA";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <FeatureShowcase />
       <Features />
       <CTA />
     </main>
